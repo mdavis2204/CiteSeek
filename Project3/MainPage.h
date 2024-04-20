d3ae484d00897dfe9595e4a12b97be725c8db754
MattD78 <109258394+MattD78@users.noreply.github.com>
2024-04-19 20:16:38 -0400

    moved readFile() call

--- Project3/MainPage.h
+++ Project3/MainPage.h
@@ -776,9 +776,6 @@ namespace Project3 {
     // FUNCTIONS I WROTE ----------------------------------------------------------------------------------------------------------
     // sorts the vector using the two methods and then searches using binary search until no elements remain in the list that have that attribute. these removed objects are placed into the class vector (the modified one)
     void MultiSort() {
-	    // read in and store csv file
-		ParkingCitations obj1;
-	    	obj1.readFile();
 	    // empty vector to store vector after binary search
 		std::vector<ParkingCitation> searched_vector = {};
         if (inputplateNumber != "") {
