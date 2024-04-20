47f9a0b587c7ef3d2b828092cf3f63036e4572cc
nick110303 <ngionti@ufl.edu>
2024-04-20 03:43:11 -0400

    Last Citation Update
    Forgot to cite a few pictures used, this should be the last commit for the night

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -8,6 +8,8 @@ using namespace System::Windows::Forms;
 /*
 CITATIONS:
 
+INSTALL AND CODE TUTORIALS
+
 C++ GUI: Install C++ CLR Workloads in Visual Studio | WinForms by Sciber
 	Used for intalling the C++ version of Winforms using VS
 https://www.youtube.com/watch?v=HcxlYkU8aY0&list=PL2i17lRog5pBe7t9zJdFdugQ6bxgjntJD&index=2
@@ -23,6 +25,15 @@ https://learn.microsoft.com/en-us/dotnet/desktop/winforms/event-handlers-overvie
 Hiding a form and showing another when a button is clicked in a Windows Forms application
 	Used to find out how to open one form and "close" another by hiding it.
 https://stackoverflow.com/questions/4345666/hiding-a-form-and-showing-another-when-a-button-is-clicked-in-a-windows-forms-ap
+
+IMAGES USED:
+
+Welcome Page Background received from here:
+https://www.criminalandduilawofgeorgia.com/2020/07/10/traffic-violation-or-criminal-charge-is-there-a-difference/
+
+Result Page Background received from here:
+https://www.pexels.com/search/parking%20lot/
+
 */
 
 
